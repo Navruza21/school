3393bbd96f23fed30816620f7e06bc742706b890
Norboboyeva Lobar <lobarxon311@gmail.com>
2024-08-14 09:52:05 +0500

    edit test push

--- src/pages/dashboard.tsx
+++ src/pages/dashboard.tsx
@@ -98,6 +98,7 @@ const Dashboard: React.FC = () => {
 
   return (
     <div className="col-span-12 rounded-sm border border-stroke bg-white p-7.5 shadow-default dark:border-strokedark dark:bg-boxdark xl:col-span-4">
+      <h1>test </h1>
       <div className="mb-4 justify-between gap-4 sm:flex">
         <h4 className="text-xl font-semibold text-black dark:text-white">
           Students Reyting chart
